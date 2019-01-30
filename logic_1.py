18091eb57558c09c5e2d8e3ca4cb50199d9c67fb
fstevanus <dracbros@gmail.com>
2019-01-30 07:11:45 +0700

    commit logic 1

---
+++ logic_1.py
@@ -0,0 +1,109 @@
+from util import print2d, create_array_2d_fill
+
+# print("n = "),
+# n = input()
+# fill = input("fill with ")
+n = 9
+fill = "-"
+
+a = create_array_2d_fill(n, fill)
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            a[i][j] = "*"
+
+print2d(a, n)
+print("")
+
+a = create_array_2d_fill(n, fill)
+for i in range(n):
+    for j in range(n):
+        if i + j == n - 1:
+            a[i][j] = "*"
+
+print2d(a, n)
+print("")
+
+a = create_array_2d_fill(n, fill)
+for i in range(n):
+    for j in range(n):
+        if i + j == n - 1 or i == j:
+            a[i][j] = "*"
+
+print2d(a, n)
+print("")
+
+a = create_array_2d_fill(n, fill)
+for i in range(n):
+    for j in range(n):
+        if i == n/2 or j == n/2:
+            a[i][j] = "*"
+
+print2d(a, n)
+print("")
+
+a = create_array_2d_fill(n, fill)
+for i in range(n):
+    for j in range(n):
+        if i == n-1 or j == n-1 or i == 0 or j == 0:
+            a[i][j] = "*"
+
+print2d(a, n)
+print("")
+
+a = create_array_2d_fill(n, fill)
+for i in range(n):
+    for j in range(n):
+        if j <= n/2:
+            if j + i == n/2:
+                a[i][j] = "*"
+                a[n-1-i][j] = "*"
+                a[i][n-1-j] = "*"
+                a[n-1-i][n-1-j] = "*"
+
+        
+print2d(a, n)
+print("")
+
+a = create_array_2d_fill(n, fill)
+for i in range(n):
+    for j in range(n):
+        if i == n-1 or j == n-1 or i == 0 or j == 0 or i + j == n - 1 or i == j:
+            a[i][j] = "*"
+
+print2d(a, n)
+print("")
+
+a = create_array_2d_fill(n, fill)
+for i in range(n):
+    for j in range(n):
+        if i == n/2 or j == n/2 or i + j == n - 1 or i == j:
+            a[i][j] = "*"
+
+print2d(a, n)
+print("")
+
+a = create_array_2d_fill(n, fill)
+for i in range(n):
+    for j in range(n):
+        if i == n-1 or j == n-1 or i == 0 or j == 0 or i == n/2 or j == n/2 or i + j == n - 1 or i == j:
+            a[i][j] = "*"
+
+print2d(a, n)
+print("")
+
+a = create_array_2d_fill(n, fill)
+for i in range(n):
+    for j in range(n):
+        if j <= n/2:
+            if j + i == n/2:
+                a[i][j] = "*"
+                a[n-1-i][j] = "*"
+                a[i][n-1-j] = "*"
+                a[n-1-i][n-1-j] = "*"
+        
+        if i == n-1 or j == n-1 or i == 0 or j == 0 or i == n/2 or j == n/2 or i + j == n - 1 or i == j:
+            a[i][j] = "*"
+        
+print2d(a, n)
+print("")
---
+++ util.py
@@ -0,0 +1,41 @@
+# fs
+
+def create_array_2d(n):
+    return [[0] * n for a in range(n)]
+
+def create_array_2d_ij(i, j):
+    return [[0] * j for a in range(i)]
+
+def create_array_2d_fill(n, fill):
+    return [[fill] * n for a in range(n)]
+
+def create_array_2d_fill_ij(i, j, fill):
+    return [[fill] * j for a in range(i)]
+
+def print2d(a, n):
+    for i in range(n):
+        for j in range(n):
+            print(a[i][j]),
+        print("")
+
+def fibonanci(n):
+    a = [0] * n
+    for i in range(n):
+        if i > 1:
+            a[i] = a[i-1] + a[i-2]
+        else:
+            a[i] = 1
+    return a
+
+def fibonanci_start_with(start_with, n):
+    a = [0] * n
+    for i in range(n):
+        if i > 1:
+            a[i] = a[i-1] + a[i-2]
+        else:
+            a[i] = start_with
+            if start_with == 0:
+                start_with += 1
+    return a
+
+# fs
